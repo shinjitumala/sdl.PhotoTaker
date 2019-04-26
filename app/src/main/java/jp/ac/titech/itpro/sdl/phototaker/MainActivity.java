c02ebeb2a5ed612e7e96703c480e0622778bad1b
shinjitumala <funny.pig.run@gmail.com>
2019-04-26 17:02:16 +0900

    高画質での写真表示を実装。FileProviderを用いた。

--- app/src/main/java/jp/ac/titech/itpro/sdl/phototaker/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/phototaker/MainActivity.java
@@ -3,7 +3,10 @@ package jp.ac.titech.itpro.sdl.phototaker;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,12 +14,26 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static int REQ_PHOTO = 1234;
     private Bitmap photoImage = null;
+
+    // creates image file for the camera
+    private File create_photo_file() throws IOException {
+        File directory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image_file = File.createTempFile("temp", ".jpg", directory);
+        image_path = image_file.getAbsolutePath();
+        return image_file;
+    }
+
+    // file path for the temporary image
+    private String image_path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +50,17 @@ public class MainActivity extends AppCompatActivity {
                 PackageManager manager = getPackageManager();
                 List activities = manager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
                 if (!activities.isEmpty()) {
+                    // create temporary file to save the image
+                    File image;
+                    try{
+                        image = create_photo_file();
+                    }catch(IOException e){
+                        Toast.makeText(MainActivity.this, "Failed to create tmp file for the image", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+
+                    Uri uri = FileProvider.getUriForFile(getApplicationContext(), "sdl.android.fileprovider", image);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
                     startActivityForResult(intent, REQ_PHOTO);
                 } else {
                     Toast.makeText(MainActivity.this, R.string.toast_no_activities, Toast.LENGTH_LONG).show();
@@ -56,9 +84,9 @@ public class MainActivity extends AppCompatActivity {
             case REQ_PHOTO:
                 if (resCode == RESULT_OK) {
                     // TODO: You should implement the code that retrieve a bitmap image
-                    Bundle bundle = data.getExtras();
-                    photoImage = (Bitmap) bundle.get("data");
-                    showPhoto();
+                    Uri image = Uri.fromFile(new File(image_path));
+                    ImageView view = findViewById(R.id.photo_view);
+                    view.setImageURI(image);
                 }
                 break;
         }
